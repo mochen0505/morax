18dcd624cf7f7d21c212b33f0de29ca93d892e0c
Mo Chen <mochen0505@hotmail.com>
2022-02-03 00:12:11 +0800

    fix: minor

--- packages/shield-perf/lib/index.js
+++ packages/shield-perf/lib/index.js
@@ -1,5 +1,3 @@
-import 'web-vitals'
-
 const reportWebVitals = onPerfEntry => {
     if (onPerfEntry && onPerfEntry instanceof Function) {
         import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
